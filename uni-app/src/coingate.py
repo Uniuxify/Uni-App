bde84cfedf936d57bf20f3c19c981100b2537e46
Uniuxify <Uniuxify@gmail.com>
2021-05-18 14:29:02 +0300

    added currency list function

--- uni-app/src/coingate.py
+++ uni-app/src/coingate.py
@@ -1,5 +1,14 @@
 import requests
+import json
 
 
 def get_rate(source, quote):
     return float(requests.get(f'https://api.coingate.com/v2/rates/merchant/{source}/{quote}').content.decode('UTF-8'))
+
+
+def get_currency_list():
+    currency_list = []
+    json_obj = json.loads(requests.get(f'https://api.coingate.com/v2/currencies').content.decode('UTF-8'))
+    for currency_json in json_obj:
+        currency_list.append({currency_json["title"]: currency_json["symbol"]})
+    return currency_list
